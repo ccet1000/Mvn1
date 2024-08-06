b0e986914025d6ff7f8541a6a67f72be41165a90
PRAKASH <PRAKASH@DESKTOP-KNS1RQQ>
2024-08-06 22:00:43 +0530

    1 commit

---
+++ MVN1/src/test/java/com/actitime/test/CreateCuntomer.java
@@ -0,0 +1,11 @@
+package com.actitime.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCuntomer {
+@Test
+public void createCustomer() {
+	Reporter.log("IS PaSsed", true);
+}
+}
